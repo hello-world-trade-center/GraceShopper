87330364b7449b39228249b0cc38a6850690bba5
Daniel <djumelle@oldwestbury.edu>
2019-11-04 15:56:03 -0500

    finished up model declarations

---
+++ server/db/models/product.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  origin: {
+    type: Sequelize.STRING
+  },
+  rating: {
+    type: Sequelize.FLOAT,
+    validate: {
+      min: 0,
+      max: 5
+    }
+  }
+})
+
+module.exports = Product
