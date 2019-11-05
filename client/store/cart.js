33fa456c840a7b2671345344c67a306724d527ba
Daniel <djumelle@oldwestbury.edu>
2019-11-05 12:27:29 -0500

    added some routes

--- client/store/cart.js
+++ client/store/cart.js
@@ -11,12 +11,30 @@ const cart = []
 //ACTION CREATORS
 const addToCart = product => ({type: ADDED_TO_CART, product})
 const removeFromCart = product => ({type: REMOVED_FROM_CART, product})
-const buyCart = () => ({type: BOUGHT_CART})
+const boughtCart = () => ({type: BOUGHT_CART})
+
+export const buyCart = () => {
+  return async dispatch => {
+    try {
+      cart.forEach(item => {
+        // await axios.post(`/api/${item.id}`)
+        // await axios.post(`/api${user.id}`, item)
+        dispatch(boughtCart())
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
 
 const cartReducer = (state = cart, action) => {
   switch (action.type) {
     case ADDED_TO_CART:
       return [...state, action.product]
+    case REMOVED_FROM_CART:
+      return state.filter(item => item.id !== action.product.id)
+    case BOUGHT_CART:
+      return []
     default:
       return state
   }
