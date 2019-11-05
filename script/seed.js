2d80bae83a804422dcb3a0ae6eafd479056abe62
Vanessa Chan <vchan8084@gmail.com>
2019-11-05 11:35:14 -0500

    products created and added imageUrl to product model

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,29 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Product} = require('../server/db/models')
+
+const products = [
+  {
+    name: 'Vitelotte',
+    price: '199.99',
+    description:
+      'Vitelotte, is a gourmet French variety of blue-violet potato. It has been cultivated in France at least since the early 19th century.',
+    origin: 'France',
+    rating: 4.8,
+    imageUrl:
+      'http://cdn.webshopapp.com/shops/145216/files/240944855/potatoes-vitelotte-purple-per-100-gram.jpg'
+  },
+  {
+    name: 'Belle de Fontenay',
+    price: '89.97',
+    description:
+      'A beautiful French first-early potato variety firm and waxy in texture. Perfect for Sunday lunches in late spring to early summer.',
+    origin: 'France',
+    rating: 4.1,
+    imageUrl: 'http://www.doreoc.com/wp-content/uploads/2014/05/charlotte.jpg'
+  }
+]
 
 async function seed() {
   await db.sync({force: true})
