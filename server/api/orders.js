39dd78dc26eed3ac969aa8e5edb6e41a1e5fe684
Daniel <djumelle@oldwestbury.edu>
2019-11-08 15:20:20 -0500

    added order routes

--- server/api/orders.js
+++ server/api/orders.js
@@ -9,7 +9,31 @@ router.get('/:orderId', async (req, res, next) => {
       }
     })
   } catch (err) {
-    console.error(err)
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newOrder = await Order.create(req.body, {
+      include: {
+        model: Product
+      }
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/:orderId', async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.id)
+    const updatedOrder = await order.update({
+      ...order,
+      complete: true
+    })
+  } catch (error) {
+    next(error)
   }
 })
 
