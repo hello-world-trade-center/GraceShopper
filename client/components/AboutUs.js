ee4397b9d5f052fe4cbc04f5c080299b784e9093
Kaitlyn Martinez <kkmartinez95@gmail.com>
2019-11-10 01:22:26 -0500

    Added our GitHub links, img & paragraph about founders still needs CSS work

---
+++ client/components/AboutUs.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className="aboutus-container">
+      {/* <div className="founderimg-and-info">
+        <h2>The Founders</h2>
+        <div className="cropped-founders-img">
+          <img src="https://www.picclickimg.com/d/l400/pict/173943410534_/Toy-Story-2-Disney-black-white.jpg" />
+        </div>
+        <div className="founders-info">
+          <h3>Our Story</h3>
+          <p>
+            <em>Pommes de Terre</em> is a luxery boutique offering the most
+            exotic and prestiguous genus of spuds. It was founded by Mr.
+            Potato-Head and his wife in 2019. Together, their mission was to
+            bring nobel potatoes to homes worthy of their pedigre.
+          </p>
+        </div>
+      </div> */}
+
+      <div className="collaborators">
+        <h2>Collaborators</h2>
+
+        <h3>Daniel Jumelles</h3>
+        <a href="https://github.com/dan2298">github.com/dan2298</a>
+        <h3>Vanessa Chan</h3>
+        <a href="https://github.com/vchan8084">github.com/vchan8084</a>
+        <h3>John Tieu</h3>
+        <a href="https://github.com/jt5301">github.com/jt5301</a>
+        <h3>Kaitlyn Martinez</h3>
+        <a href="https://github.com/kkmartinez95">github.com/kkmartinez95</a>
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
